b6dbaf8fbf8fcfe2ca1ba94c90104ab63cb28cdf
ekibun <soekibun@gmail.com>
2019-03-10 21:24:02 +0800

    fix assist TIM

--- app/src/main/java/soko/ekibun/qqnotifyplus/service/AccessibilityMonitorService.kt
+++ app/src/main/java/soko/ekibun/qqnotifyplus/service/AccessibilityMonitorService.kt
@@ -14,25 +14,43 @@ class AccessibilityMonitorService : AccessibilityService() {
         when (event.eventType) {
             AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED ->{
                 val tag = NotificationMonitorService.tags[currentPackage]?:return
-                if ("com.tencent.mobileqq.activity.SplashActivity" == currentActivity || "com.dataline.activities.LiteActivity" == currentActivity)
-                    enumNode(rootInActiveWindow?:return, "root|"){subNode->
-                        if(subNode.contentDescription in listOf("群资料卡", "返回消息")) {
-                            enumNode(subNode.parent, "toolbar|") {node->
-                                if(node.className == "android.widget.TextView" && !node.text.isNullOrEmpty()) {
-                                    startService(Intent(this, NotificationMonitorService::class.java).putExtra("key", "${tag.name}_${node.text}"))
-                                    true
-                                } else false
-                            }
-                            true
-                        } else false
-                    }
-                else if (currentActivity.startsWith("cooperation.qzone."))
+                if (currentActivity.startsWith("cooperation.qzone."))
                     enumNode(rootInActiveWindow?:return, "root|"){node->
                         if(node.contentDescription == "消息 标题") {
                             startService(Intent(this, NotificationMonitorService::class.java).putExtra("key", "${NotificationMonitorService.qzoneTag[tag]?:tag}_qzone"))
                             true
                         } else false
                     }
+                else when(currentPackage){
+                    "com.tencent.qqlite", "com.tencent.mobileqq" -> {
+                        if(currentActivity in listOf("com.tencent.mobileqq.activity.SplashActivity", "com.dataline.activities.LiteActivity"))
+                            enumNode(rootInActiveWindow?:return, "root|"){subNode->
+                                if(subNode.contentDescription in listOf("群资料卡", "返回消息")) {
+                                    enumNode(subNode.parent, "toolbar|") {node->
+                                        if(node.className == "android.widget.TextView" && !node.text.isNullOrEmpty()) {
+                                            startService(Intent(this, NotificationMonitorService::class.java).putExtra("key", "${tag.name}_${node.text}"))
+                                            true
+                                        } else false
+                                    }
+                                    true
+                                } else false
+                            }
+                    }
+                    "com.tencent.tim" -> {
+                        if(currentActivity in listOf("com.tencent.mobileqq.activity.SplashActivity", "com.tencent.mobileqq.activity.ChatActivity"))
+                            enumNode(rootInActiveWindow?:return, "root|"){subNode->
+                                if(subNode.contentDescription in listOf("返回消息界面", "群资料卡")) {
+                                    enumNode(subNode.parent.parent?:subNode.parent, "toolbar|") {node->
+                                        if(node.className == "android.widget.TextView" && !node.text.isNullOrEmpty() && node.contentDescription == null) {
+                                            startService(Intent(this, NotificationMonitorService::class.java).putExtra("key", "${tag.name}_${node.text}"))
+                                            true
+                                        } else false
+                                    }
+                                    true
+                                } else false
+                            }
+                    }
+                }
             }
             AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED -> {
                 if (event.packageName == null || event.className == null) return
