c7e3f5efb2bf1f062d15b8ad20a019cc71697ab4
ekibun <soekibun@gmail.com>
2019-03-30 15:41:53 +0800

    add activity?

---
+++ app/src/main/java/soko/ekibun/qqnotifyplus/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package soko.ekibun.qqnotifyplus.activity
+
+import android.app.Activity
+import android.os.Bundle
+import soko.ekibun.qqnotifyplus.R
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
